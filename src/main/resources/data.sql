d1221687f2e8284a57b1823651d05dd5ee9900c2
UO272563 <uo272563@uniovi.es>
2021-03-24 12:14:39 +0100

    Base bien

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,41 @@
 delete from instalaciones; 
-INSERT INTO instalaciones (id_instalacion, nombre, precio, aforo) VALUES (1,'Pista de tenis', 5, 10); 
+INSERT INTO instalaciones (id_instalacion, nombre, precio, aforo, plazo_maximo_reserva, hora_maxima_reserva) VALUES (1,'Pista de tenis', 5, 10,30,4); 
+INSERT INTO instalaciones (id_instalacion, nombre, precio, aforo, plazo_maximo_reserva, hora_maxima_reserva) VALUES (2,'Pista de pádel', 5, 10,30,4); 
+INSERT INTO instalaciones (id_instalacion, nombre, precio, aforo, plazo_maximo_reserva, hora_maxima_reserva) VALUES (3,'Cancha de baloncesto', 5, 10,30,4); 
+INSERT INTO instalaciones (id_instalacion, nombre, precio, aforo, plazo_maximo_reserva, hora_maxima_reserva) VALUES (4,'Campo de fútbol', 5, 10,30,4); 
+INSERT INTO instalaciones (id_instalacion, nombre, precio, aforo, plazo_maximo_reserva, hora_maxima_reserva) VALUES (5,'Pista de fútbol sala', 5, 10,30,4);
+INSERT INTO instalaciones (id_instalacion, nombre, precio, aforo, plazo_maximo_reserva, hora_maxima_reserva) VALUES (6,'Pista multideporte', 5, 10,30,4); 
+
+delete from horarioActividad;
+INSERT INTO horarioActividad(id_horario, id_actividad)
+VALUES (1,1);
 
 delete from actividades; 
-INSERT INTO actividades (id_actividad, id_instalacion, nombre, aforo, cuota_socio, cuota_no_socio, fechaInicioActividad, fechaFinActividad, id_inscripcion, id_horario) VALUES (1,1,'Baloncesto', 50, 20, 25,'2019-04-01', '2019-04-30', 1, 1); 
+INSERT INTO actividades (id_actividad, id_instalacion, nombre, aforo, cuota_socio, cuota_no_socio, fechaInicioActividad, fechaFinActividad, id_inscripcion) VALUES (1,1,'Baloncesto', 50, 20, 25,'01/04/2019', '25/04/2021', 1); 
+
 
 delete from socios;
-INSERT INTO socios (dni, nombre, apellidos) VALUES ('12345678X','Ana', 'Perez Gomez');
+INSERT INTO socios (id_socio, dni, nombre, apellidos,cuenta, telefono) VALUES (1,'12345678X','Ana', 'Perez Gomez','123456789IBAN', 123456789);
 
 delete from noSocios;
-INSERT INTO noSocios (dni, nombre, apellidos) VALUES ('87654321X','Juan', 'Gomez Perez');
+INSERT INTO noSocios (id_noSocio,dni, nombre, apellidos) VALUES (1,'87654321X','Juan', 'Gomez Perez');
 
 delete from inscripciones;
-INSERT INTO inscripciones (id_inscripcion, fechaInicioSocios, fechaFinSocios, fechaFinNoSocios) VALUES (1,'01/06/2019','01/07/2019','30/08/2019');
+INSERT INTO inscripciones (id_inscripcion, nombre, fechaInicioSocios, fechaFinSocios, fechaFinNoSocios) VALUES (1, 'Primer cuatri','01/06/2019','01/07/2019','30/08/2019');
 
 delete from horario;
-INSERT INTO horario (id_horario,dia,hora_ini,hora_fin) VALUES (1, "Lunes", "13:00", "19:30");
+INSERT INTO horario (id_horario,dia,hora_ini,hora_fin) VALUES (1, "Lunes", "13:00", "19:00");
 
 delete from reservas;
-INSERT INTO reservas (id_reserva, fechaIni, fechaFin, id_actividad) VALUES (1, "9:00", "10:00", 1);
+INSERT INTO reservas (id_reserva, fechaIni, fechaFin, id_actividad, id_instalacion, hora_ini, hora_fin,id_socio) VALUES (1, "01/04/2021", "01/04/2021", 1, 1, "9:00", "10:00",1);
+
+delete from contabilidad;
+INSERT INTO contabilidad (id_contabilidad, id_socio, importe, descripcion, pendiente, fecha) VALUES (1,1,10, 'reserva piscina', 'pagado', '2019-04-22');
+
+
+
+
+
+
+
+
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,12 +6,24 @@ drop table inscripciones;
 drop table horario;
 drop table actividades;
 drop table reservas;
+drop table horarioActividad;
+drop table contabilidad;
+
+
+CREATE TABLE horarioActividad (
+id_horario INTEGER,
+id_actividad INTEGER,
+PRIMARY KEY(
+	id_horario,
+	id_actividad));
 
 CREATE TABLE instalaciones (
 id_instalacion INTEGER PRIMARY KEY AUTOINCREMENT,
 nombre TEXT NOT NULL,
 precio TEXT NOT NULL,
-aforo INTEGER NOT NULL);
+aforo INTEGER NOT NULL,
+plazo_maximo_reserva INTEGER NOT NULL,
+hora_maxima_reserva INTEGER NOT NULL);
 
 
 CREATE TABLE actividades (
@@ -23,28 +35,31 @@ cuota_no_socio TEXT NOT NULL,
 fechaInicioActividad TEXT NOT NULL, 
 fechaFinActividad TEXT NOT NULL, 
 id_inscripcion INTEGER, 
-id_horario INTEGER,
 FOREIGN KEY (id_instalacion) REFERENCES instalaciones(id_instalacion), 
-FOREIGN KEY (id_inscripcion) REFERENCES inscripciones (id_inscripcion),
-FOREIGN KEY (id_horario) REFERENCES horario (id_horario));
+FOREIGN KEY (id_inscripcion) REFERENCES inscripciones (id_inscripcion));
 
 
 CREATE TABLE socios(
-dni TEXT NOT NULL PRIMARY KEY, 
+id_socio INTEGER PRIMARY KEY AUTOINCREMENT, 
+dni TEXT NOT NULL, 
 nombre TEXT NOT NULL, 
-apellidos TEXT NOT NULL);
+apellidos TEXT NOT NULL,
+cuenta TEXT NOT NULL,
+telefono INTEGER NOT NULL);
 
 
 
 CREATE TABLE noSocios(
-dni TEXT NOT NULL PRIMARY KEY, 
+id_noSocio INTEGER PRIMARY KEY AUTOINCREMENT,
+dni TEXT NOT NULL, 
 nombre TEXT NOT NULL, 
 apellidos TEXT NOT NULL);
 
 
 
 CREATE TABLE inscripciones (
-id_inscripcion INTEGER PRIMARY KEY AUTOINCREMENT, 
+id_inscripcion INTEGER PRIMARY KEY AUTOINCREMENT,
+nombre TEXT NOT NULL,
 fechaInicioSocios TEXT NOT NULL, 
 fechaFinSocios TEXT NOT NULL, 
 fechaFinNoSocios TEXT NOT NULL);
@@ -63,4 +78,21 @@ id_reserva INTEGER PRIMARY KEY AUTOINCREMENT,
 fechaIni TEXT NOT NULL, 
 fechaFin TEXT NOT NULL,
 id_actividad INTEGER, 
-FOREIGN KEY (id_actividad) REFERENCES actividades (id_actividad));
+id_instalacion INTEGER,
+hora_ini TEXT NOT NULL,
+hora_fin TEXT NOT NULL,
+id_socio INTEGER,
+FOREIGN KEY (id_actividad) REFERENCES actividades (id_actividad),
+FOREIGN KEY (id_instalacion) REFERENCES instalaciones (id_instalacion),
+FOREIGN KEY (id_socio) REFERENCES socios (id_socio));
+
+
+CREATE TABLE contabilidad (
+	id_contabilidad INTEGER PRIMARY KEY AUTOINCREMENT,
+	id_socio INTEGER,
+	importe INTEGER NOT NULL,
+	descripcion TEXT NOT NULL,
+	pendiente TEXT NOT NULL,
+	fecha TEXT NOT NULL,
+	FOREIGN KEY (id_socio) REFERENCES socios (id_socio)
+	);
