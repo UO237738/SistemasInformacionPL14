ffce067340eeee461dab332839f592b2acb6732d
UO272563 <uo272563@uniovi.es>
2021-03-24 12:22:02 +0100

    bien

--- src/main/java/Vista/DateLabelFormatter.java
+++ src/main/java/Vista/DateLabelFormatter.java
@@ -1,4 +1,3 @@
-
 package Vista;
 
 import java.text.ParseException;
@@ -32,38 +31,3 @@ public class DateLabelFormatter extends AbstractFormatter {
     }
 
 }
-=======
-package Vista;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-
-import javax.swing.JFormattedTextField.AbstractFormatter;
-
-/** Clase para dar formato a los calendarios JDatePicker
- * 
- * @author peibol
- */
-public class DateLabelFormatter extends AbstractFormatter {
-
-    private String datePattern = "yyyy-MM-dd";
-    private SimpleDateFormat dateFormatter = new SimpleDateFormat(datePattern);
-
-    @Override
-    public Object stringToValue(String text) throws ParseException {
-        return dateFormatter.parseObject(text);
-    }
-
-    @Override
-    public String valueToString(Object value) throws ParseException {
-        if (value != null) {
-            Calendar cal = (Calendar) value;
-            return dateFormatter.format(cal.getTime());
-        }
-
-        return "";
-    }
-
-}
-
