1e444a8caa8a0f100ac4c1d4865d4fc0f70439da
UO272563 <uo272563@uniovi.es>
2021-04-13 15:52:26 +0200

    xx

--- src/main/java/Controlador/ControladorListarActividadesAdministracion.java
+++ src/main/java/Controlador/ControladorListarActividadesAdministracion.java
@@ -3,11 +3,16 @@ package Controlador;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
+
+import Modelo.ModeloCrearActividadDisplayDTO;
 import Modelo.ModeloListarActividadesAdministracion;
 import Vista.VistaListarActividadesAdministracion;
 import giis.demo.util.SwingUtil;
@@ -15,7 +20,6 @@ import giis.demo.util.Util;
 
 public class ControladorListarActividadesAdministracion {
 	
-	private ModeloListarActividadesAdministracion listar;
 	private VistaListarActividadesAdministracion vista;
 
 	
@@ -27,6 +31,7 @@ public class ControladorListarActividadesAdministracion {
 	
 	public void initview() {
 		vista.getFrame().setVisible(true);
+		//mostrarActividades();
 	}
 	
 	public static String getFechaActual() {
@@ -35,32 +40,65 @@ public class ControladorListarActividadesAdministracion {
 		return formateador.format(ahora);
 	}
 	
+	public  void reiniciarTabla(){
+		DefaultTableModel modelo = (DefaultTableModel) vista.JTActividades.getModel();
+		while(modelo.getRowCount()>0)modelo.removeRow(0);
+	}
+	
 	
 	private void addListernerCRA() {
 		vista.JBBuscar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				getListaActividades();
+				reiniciarTabla();
+				mostrarActividadesFecha();
 				}
 				
 		});
 		
 	}
 	
+	
+	public void mostrarActividades() {
+		ArrayList<ModeloCrearActividadDisplayDTO> list= ModeloListarActividadesAdministracion.listarActividades();
+		DefaultTableModel model= (DefaultTableModel)vista.JTActividades.getModel();
 
-	
-	
-	private void getListaActividades(){
-		List<Object[]> actividades = ModeloListarActividadesAdministracion.getListaActividades(Util.isoStringToDate(vista.getJDFechaini()), Util.isoStringToDate(vista.getJDFechafin()));
-		TableModel tmodel = SwingUtil.getTableModelFromPojos(actividades, new String[] {"id_actividad", "nombre", "fechaIniActividad", "aforo", "cuota socio", "cuota no socio"});
-		vista.gettableactividades().setModel(tmodel);
-		
-		vista.gettableactividades().getColumnModel().getColumn(0).setHeaderValue("ID actividad");
-		vista.gettableactividades().getColumnModel().getColumn(1).setHeaderValue("Nombre");
-		vista.gettableactividades().getColumnModel().getColumn(2).setHeaderValue("Fecha");
-		vista.gettableactividades().getColumnModel().getColumn(3).setHeaderValue("Aforo");
-		vista.gettableactividades().getColumnModel().getColumn(4).setHeaderValue("Cuota Socio");
-		vista.gettableactividades().getColumnModel().getColumn(5).setHeaderValue("cuota No Socio");
+		Object [] row = new Object[7];
+		for (int i=0;i<list.size();i++) {
+			row[0]=list.get(i).getId_instalacion();
+			row[1]=list.get(i).getNombre();
+			row[2]=list.get(i).getAforo();
+			row[3]=list.get(i).getCuota_socio();
+			row[4]=list.get(i).getCuota_no_socio();
+			row[5]=list.get(i).getFechaInicioActividad();
+			row[6]=list.get(i).getFechaFinActividad();
+
+			model.addRow(row);
+		}
 	}
+	
+	
+	public void mostrarActividadesFecha() {
+		Date fi = Util.isoStringToDate(vista.JDFechaini.getJFormattedTextField().getText());
+		Date ff = Util.isoStringToDate(vista.JDFechafin.getJFormattedTextField().getText());
+
+		ArrayList<ModeloCrearActividadDisplayDTO> list= ModeloListarActividadesAdministracion.listarActividadesFecha(fi, ff);
+		DefaultTableModel model= (DefaultTableModel)vista.JTActividades.getModel();
+
+		Object [] row = new Object[7];
+		for (int i=0;i<list.size();i++) {
+			row[0]=list.get(i).getId_instalacion();
+			row[1]=list.get(i).getNombre();
+			row[2]=list.get(i).getAforo();
+			row[3]=list.get(i).getCuota_socio();
+			row[4]=list.get(i).getCuota_no_socio();
+			row[5]=list.get(i).getFechaInicioActividad();
+			row[6]=list.get(i).getFechaFinActividad();
+
+			model.addRow(row);
+		}
+	}
+	
+	
 	
 	
 	
